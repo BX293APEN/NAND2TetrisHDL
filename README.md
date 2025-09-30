98500ab75a313ed93122a97f8f6064ef0d6a68e3
PEN <uemasaki20011222@gmail.com>
2025-09-30 18:59:16 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NAND2TetrisHDL
 NAND2TetrisのHDLを簡単に書く
+
+# References
+> [NAND2Tetris](https://nand2tetris.github.io/web-ide/chip/)
---
+++ create_chip.py
@@ -0,0 +1,62 @@
+class Chip:
+    def __init__(self, name, bit=1):
+        self.name = name
+        if bit >1:
+            self.bit = f"{bit}"
+        else:
+            self.bit = ""
+        self.inputs = []
+        self.outputs = []
+        self.parts = []
+
+    def chip_io(self, wireInput=[], wireOutput=[]):
+        self.inputs = wireInput
+        self.outputs = wireOutput
+
+    def add_function(
+        self, 
+        chipName, 
+        inputChip = [], inputWire =[], 
+        outChip = [], outWire = [],
+        lsb = 0, msb = 0,
+    ):
+        if (msb - lsb) == 0:
+            args = []
+            for inputPair in zip(inputChip, inputWire):
+                args.append(f"{inputPair[0]}={inputPair[1]}")
+            
+            for outPair in zip(outChip, outWire):
+                args.append(f"{outPair[0]}={outPair[1]}")
+
+            self.parts.append(f"{chipName}({", ".join(args)});")
+        
+        else:
+            for b in range(lsb, msb+1, 1):
+                args = []
+                for inputPair in zip(inputChip, inputWire):
+                    args.append(f"{inputPair[0]}={inputPair[1]}[{b}]")
+                
+                for outPair in zip(outChip, outWire):
+                    args.append(f"{outPair[0]}={outPair[1]}[{b}]")
+
+                self.parts.append(f"{chipName}({", ".join(args)});")
+
+    def dump(self, path):
+        hdlData = f"""// {self.name} CHIP
+CHIP {self.name}{self.bit} {{
+    IN {', '.join(self.inputs)};
+    OUT {', '.join(self.outputs)};
+    PARTS:
+        {"\n        ".join(self.parts)}
+}}"""
+        
+        with open(path, "w", encoding="UTF-8") as f:
+            f.write(hdlData)
+
+
+if __name__ == "__main__":
+    andChip = Chip(name="And")
+    andChip.chip_io(["a", "b"], ["out"])
+    andChip.add_function("Nand", ["a", "b"], ["a", "b"], ["out"], ["w1"])
+    andChip.add_function("Not", ["in"], ["w1"], ["out"], ["out"])
+    andChip.dump("And.thdl")
