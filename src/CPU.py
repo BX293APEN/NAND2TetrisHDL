6d4930d01ea9c27a70097291c96d6ef0da8944af
PEN <uemasaki20011222@gmail.com>
2025-10-30 22:23:45 +0900

    20251030

--- src/CPU.py
+++ src/CPU.py
@@ -17,47 +17,175 @@ if __name__ == "__main__":
             "pc[15]"
         ]
     )
+    cpu.add_function(
+        "Not",
+        ["in"],
+        ["instruction[15]"],
+        ["out"],
+        ["instA"]
+    )
 
-    cpu.add_comment("CPUを作る")
+    cpu.add_function(
+        "Not",
+        ["in"],
+        ["instA"],
+        ["out"],
+        ["instC"]
+    )
 
     cpu.add_function(
         "Mux16",
         ["a", "b", "sel"], 
-        ["instruction", "aluOut", "instruction[15]"]
-        ["out"]
+        ["instruction", "aluOut", "instC"],
+        ["out"],
         ["instOut"]
+    )
+
+    cpu.add_function(
+        "Or",
+        ["a", "b"],
+        ["instA", "instruction[5]"],
+        ["out"],
+        ["aLoad"]
     )
 
     cpu.add_function( # Aレジスタ
         "Register",
         ["in", "load"], 
-        ["instOut", ""]
-        ["out"]
-        ["aROut"]
+        ["instOut", "aLoad"],
+        ["out", "out[0..14]"],
+        ["aROut", "addressM[0..14]"]
+    )
+
+    cpu.add_function(
+        "And",
+        ["a", "b"],
+        ["instC", "instruction[4]"],
+        ["out"],
+        ["dLoad"]
+    )
+
+    cpu.add_function( # Dレジスタ
+        "DRegister",
+        ["in", "load"], 
+        ["aluOut", "dLoad"],
+        ["out"],
+        ["dOut"]
     )
 
     cpu.add_function(
         "Mux16",
         ["a", "b", "sel"], 
-        ["aROut", "inM", ""]
-        ["out"]
+        ["aROut", "inM", "instruction[12]"],
+        ["out"],
         ["amOut"]
     )
 
     cpu.add_function(
         "ALU",
-        ["x", "y", "zx", "nx", "zy", "ny", "f", "no"], 
-        ["dOut", "amOut", ""]
-        ["out"]
-        ["aluOut"]
+        [
+            "x", "y", 
+            "zx", "nx", "zy", 
+            "ny", "f", "no"
+        ], 
+        [
+            "dOut", "amOut", 
+            "instruction[11]", "instruction[10]", "instruction[9]", 
+            "instruction[8]", "instruction[7]", "instruction[6]"
+        ],
+        ["out", "out", "zr", "ng"],
+        ["aluOut", "outM", "zr", "ng"]
+    )
+
+    cpu.add_function(
+        "And",
+        ["a", "b"], 
+        ["instruction[3]", "instC"],
+        ["out"],
+        ["writeM"]
+    )
+
+
+    cpu.add_comment("プログラムカウンタ処理")
+
+    cpu.add_function( # ng処理
+        "And",
+        ["a", "b"],
+        ["ng", "instruction[2]"],
+        ["out"],
+        ["ngJmp"]
+    )
+
+    cpu.add_function( # zr処理
+        "And",
+        ["a", "b"],
+        ["zr", "instruction[1]"],
+        ["out"],
+        ["zrJmp"]
+    )
+
+    cpu.add_function(
+        "Or",
+        ["a", "b"],
+        ["ng", "zr"],
+        ["out"],
+        ["lezero"]
+    )
+
+    cpu.add_function(
+        "Not",
+        ["in"],
+        ["lezero"],
+        ["out"],
+        ["gtzero"]
+    )
+
+    cpu.add_function( # 0より大きい処理
+        "And",
+        ["a", "b"],
+        ["gtzero", "instruction[0]"],
+        ["out"],
+        ["gtJmp"]
+    )
+
+    cpu.add_function(
+        "Or",
+        ["a", "b"],
+        ["ngJmp", "zrJmp"],
+        ["out"],
+        ["ngzrJmp"]
+    )
+
+    cpu.add_function(
+        "Or",
+        ["a", "b"],
+        ["ngzrJmp", "gtJmp"],
+        ["out"],
+        ["jmp"]
+    )
+
+    cpu.add_function(
+        "And",
+        ["a", "b"],
+        ["jmp", "instC"],
+        ["out"],
+        ["pcLoad"]
+    )
+
+    cpu.add_function(
+        "Not",
+        ["in"],
+        ["pcLoad"],
+        ["out"],
+        ["pcInc"]
     )
 
     cpu.add_function(
         "PC",
         ["in", "load", "inc", "reset"], 
-        ["", "", "", "reset"]
-        ["out"]
-        ["pc"]
+        ["aROut", "pcLoad", "pcInc", "reset"],
+        ["out[0..14]", "out[15]"],
+        ["pc[0..14]", "pcsp"]
     )
 
     cpu.dump(f"{directory}/CPU.thdl")
